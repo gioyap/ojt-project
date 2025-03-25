9b00e03412f49039e2f3c849e674878cc315faed
unknown <delnardcantor1@gmail.com>
2025-03-25 10:30:29 +0800

    Updated Login Function and Logo Style

--- app/page.tsx
+++ app/page.tsx
@@ -11,35 +11,28 @@ export default function Home() {
       ></div>
 
       {/* Content Wrapper */}
-      <div className="relative z-10 flex flex-col items-center justify-center min-h-screen p-4">
-        <div className="text-center bg-white bg-opacity-80 p-6 rounded-lg shadow-lg">
-          <h1 className="text-4xl font-bold text-gray-900 mb-4">
+      <div className=" relative animate-in fade-in-5 duration-1000 z-10 flex flex-col items-start  w-auto justify-center min-h-screen p-8 sm:p-16 md:p-24 lg:p-60">
+        <div className="text-center bg-white bg-opacity-80 p-8 rounded-lg shadow-lg w-full sm:w-max md:w-max lg:w-max">
+          <h1 className="text-3xl sm:text-4xl font-bold text-gray-900 mb-4">
             RSC GROUP INTERNSHIP
           </h1>
-          <p className="text-lg text-gray-600 mb-8">
+          <p className="text-base sm:text-lg text-gray-600 mb-8">
             Please select your role to continue.
           </p>
-          <div className="space-y-4">
+          <div className="space-y-4 w-full">
             <Link
               href="/sign-in?role=trainee"
-              className="bg-blue-600 text-white px-8 py-4 rounded-lg shadow-lg hover:bg-blue-700 transition duration-300 text-xl font-semibold flex items-center justify-center space-x-2"
+              className="bg-red-600 text-white px-6 py-3 sm:px-8 sm:py-4 rounded-lg shadow-lg hover:bg-red-700 transition duration-300 text-lg sm:text-xl font-semibold flex items-center justify-center space-x-2"
             >
-              <FaUserGraduate className="w-6 h-6" />
+              <FaUserGraduate className="w-5 h-5 sm:w-6 sm:h-6" />
               <span>TRAINEE</span>
             </Link>
             <Link
               href="/sign-in?role=admin"
-              className="bg-purple-600 text-white px-8 py-4 rounded-lg shadow-lg hover:bg-purple-700 transition duration-300 text-xl font-semibold flex items-center justify-center space-x-2"
+              className="bg-blue-600 text-white px-6 py-3 sm:px-8 sm:py-4 rounded-lg shadow-lg hover:bg-blue-700 transition duration-300 text-lg sm:text-xl font-semibold flex items-center justify-center space-x-2"
             >
-              <FaUserShield className="w-6 h-6" />
+              <FaUserShield className="w-5 h-5 sm:w-6 sm:h-6" />
               <span>ADMIN</span>
-            </Link>
-            <Link
-              href="/sign-in?role=superadmin"
-              className="bg-yellow-600 text-white px-8 py-4 rounded-lg shadow-lg hover:bg-yellow-700 transition duration-300 text-xl font-semibold flex items-center justify-center space-x-2"
-            >
-              <FaUserCog className="w-6 h-6" />
-              <span>SUPERADMIN</span>
             </Link>
           </div>
         </div>
