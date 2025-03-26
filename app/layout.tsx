4eb433b21b657678922c63a14ea424a33934003d
MIS-GIO <yapgioedrian@gmail.com>
2025-03-26 09:13:56 +0800

    test

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import "./globals.css";
 const defaultUrl = process.env.VERCEL_URL
 	? `https://${process.env.VERCEL_URL}`
 	: "http://localhost:3000";
-
+// tabname
 export const metadata = {
 	metadataBase: new URL(defaultUrl),
 	title: "RSC InternHub",
